e015db7e8c4e93dafd1978c7357c52acac336584
Artem Kutuzov <tema.kutuzzzov@yandex.ru>
2022-06-08 00:05:35 +0300

    111

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:home'
-LOGOUT_REDIRECT_URL = 'posts:home'
+#LOGOUT_REDIRECT_URL = 'posts:home'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
